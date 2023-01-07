9073b681e46e2648ead3f907a30d75d947218159
lucabeccatini <lb_linux@VictusLB.localdomain>
2023-01-07 19:08:59 +0100

    Corrected back-propagation, added relu

--- NNHL.py
+++ NNHL.py
@@ -14,12 +14,18 @@ class NeuralNetwork():
         n_bias = np.sum([layers[i] for i in range(1, len(layers))])          # number of total bias of nn
         n_weights = np.sum([layers[i]*layers[i+1] for i in range(len(layers)-1)])    # number of total weights of nn
 
-        #self.b = np.random.rand(n_bias)                                          # bias array
         self.b = np.zeros(n_bias)                                          # bias array
-        self.w = np.random.randn(n_weights)                                           # weights array 
+        self.w = np.zeros(n_weights)
         self.n = np.zeros(layers[0]+n_bias)                                         # nodes values of last prediction, used for the back-propagation
         self.b_error = np.zeros(n_bias)                                          # corrections for biases of last prediction, used for the back-propagation
         self.w_error = np.zeros(n_weights)                                           # corrections for weights of last prediction, used for the back-propagation
+        # weights initialization
+        for i in range(1, len(self.layers)):
+            # heuristic 
+            self.w[self.ind_w([i, 0, 0]):self.ind_w([i+1, 0, 0])] = np.random.normal(0, np.sqrt(2/self.layers[i-1]), self.ind_w([i+1, 0, 0])-self.ind_w([i, 0, 0])) 
+            # Xavier 
+            #self.w[self.ind_w([i, 0, 0]):self.ind_w([i+1, 0, 0])] = np.random.normal(0, 1/self.layers[i-1], self.ind_w([i+1, 0, 0])-self.ind_w([i, 0, 0]))
+        self.activation = ['relu', 'sigmoid']
 
 
     # index of bias
@@ -186,27 +192,66 @@ class NeuralNetwork():
 
 
     # activation function
-    # consider only Sigmoid activation function
-    def activation(self, z):
-        res = 1 / (1 + np.exp(-z))
+    # available: sigmoid, relu, linear
+    def activation_func(self, z, i):
+        if (i==len(self.layers)-1):
+            if (self.activation[1]=='sigmoid'):
+                res = 1 / (1 + np.exp(-z))
+            elif (self.activation[1]=='relu'):
+                res = np.maximum(np.zeros_like(z), z)
+            elif (self.activation[1]=='linear'):
+                res = z
+        else:
+            if (self.activation[0]=='sigmoid'):
+                res = 1 / (1 + np.exp(-z))
+            elif (self.activation[0]=='relu'):
+                res = np.maximum(np.zeros_like(z), z)
+            elif (self.activation[0]=='linear'):
+                res = z
         return res
+
+
+    # partial derivative of the activation function respect the pre-activation node
+    # available: sigmoid, relu, linear
+    def der_activation(self, n, i):
+        # n: value of the node after the activation function; i: layer index
+        if (i==len(self.layers)-1):
+            if (self.activation[1]=='sigmoid'):
+                res = n * (np.ones_like(n) - n)
+            elif (self.activation[1]=='relu'):
+                res = np.heaviside(n, 0)
+            elif (self.activation[1]=='linear'):
+                res = np.ones_like(n)
+        else:
+            if (self.activation[0]=='sigmoid'):
+                res = n * (np.ones_like(n) - n)
+            elif (self.activation[0]=='relu'):
+                res = np.heaviside(n, 0)
+            elif (self.activation[0]=='linear'):
+                res = np.ones_like(n)
+        return res
+
 
     # loss function
     # consider only mse loss function
-    def loss(self, y_true):
+    def loss_func(self, y_train):
         if (self.layers[-1]==1):
-            mse = (y_true - self.node([-1, 0]))**2
+            mse = (y_train - self.node([-1, 0]))**2
         else:
             mse = 0
             for i in range(self.layers[-1]):
-                mse += (y_true[i] - self.node([-1, i]))**2
+                mse += (y_train[i] - self.node([-1, i]))**2
             mse = mse/len(self.layers)
         return mse
 
-    # derivative of the loss function
-    def der_loss(self, diff, nodes):
-        der = diff * nodes * (np.ones(self.layers[-1]) - nodes)
+
+    # partial derivative of the loss function respect the output layer
+    # consider only mse loss function
+    def der_loss(self, y_train):
+        #der = 2 * (y_train - self.node([-1]))
+        der = 2 * (self.node([-1]) - y_train)
         return der
+
 
     # predictng function
     def predict(self, x_data):
@@ -217,20 +262,10 @@ class NeuralNetwork():
     # feed-forward 
     # the results are arrays: L, which contains all the hiddane layers nodes values (needed for propagation), and y, which are the outputs
     def forward(self, X):
-        self.n[0:self.ind_n([1, 0])] = X   ###### modify not only the first of the first layer
+        self.n[0:self.ind_n([1, 0])] = X 
         for i in range(1, len(self.layers)):
-            self.n[self.ind_n([i, 0]):self.ind_n([i+1, 0])] = self.activation( np.sum(self.node([i-1]) * self.weight([i]), axis=1) + self.bias([i]) ) 
+            self.n[self.ind_n([i, 0]):self.ind_n([i+1, 0])] = self.activation_func( np.sum(self.node([i-1]) * self.weight([i]), axis=1) + self.bias([i]), i ) 
         return
-
-        """
-            for j in range(self.layers[i]):
-                if (i==1):
-                    self.node[1, j] = self.activation( np.sum(X*self.weight[0, j]) + self.bias[1, j] )
-                elif (i==(len(self.layers)-1)):
-                    self.y[j] = self.activation( np.sum(self.node[i-1] * self.weight[i-1, j]) + self.bias[i, j] )
-                else:
-                    self.node[i, j] = self.activation( np.sum(self.node[i-1] * self.weight[i-1, j]) + self.bias[i, j] )
-        """
 
 
     # back-propagation
@@ -239,19 +274,19 @@ class NeuralNetwork():
     def propagation(self, y_train):                                        # L, y, y_train are arrays
         for i in reversed(range(1, len(self.layers))): 
             if (i==(len(self.layers)-1)):
-                self.b_error[self.ind_b([i, 0]):self.ind_b([i+1, 0])] = 2 * self.der_loss(y_train - self.node([i]), self.node([i]))      # biases errors 
+                self.b_error[self.ind_b([i, 0]):self.ind_b([i+1, 0])] = self.der_loss(y_train) * self.der_activation(self.node([i]), i)     # biases errors 
                 w_err = np.outer(self.bias_error([i]), self.node([i-1]))       # weights errors
                 self.w_error[self.ind_w([i, 0, 0]):self.ind_w([i+1, 0, 0])] = np.reshape(w_err, (self.ind_w([i+1, 0, 0])-self.ind_w([i, 0, 0])))
                 # outer(a, b): a defines the row, b the column. example: w_iJ = L[l]_i * b_j. Outer result is a matrix, [:, 0] to have an array
             else: 
-                self.b_error[self.ind_b([i, 0]):self.ind_b([i+1, 0])] = self.der_loss(np.dot(self.bias_error([i+1]), self.weight_error([i+1])), self.node([i]))      # biases errors 
+                self.b_error[self.ind_b([i, 0]):self.ind_b([i+1, 0])] = np.dot(self.bias_error([i+1]), self.weight([i+1])) * self.der_activation(self.node([i]), i)      # biases errors 
                 w_err = np.outer(self.bias_error([i]), self.node([i-1]))                        # weights errors
                 self.w_error[self.ind_w([i, 0, 0]):self.ind_w([i+1, 0, 0])] = np.reshape(w_err, (self.ind_w([i+1, 0, 0])-self.ind_w([i, 0, 0])))
         return 
  
 
     # training and evaluation
-    def fit(self, X_train, Y_train, X_val, Y_val, epochs, batch_size=100, learn_rate=0.01, patience=10, min_improvement=0):
+    def fit(self, X_train, Y_train, X_val, Y_val, epochs, batch_size=100, learn_rate=0.01, patience=10, min_improvement=0, shuffle=True):
         n_train = len(X_train)
         n_val = len(X_val)
         self.epochs = epochs
@@ -263,7 +298,19 @@ class NeuralNetwork():
         corr_w = np.zeros(len(self.w))                               # to add the corrections to weights at the end of the batch
         corr_b = np.zeros(len(self.b))                               # to add the corrections to biases at the end of the batch
 
+        if (shuffle==True):
+            Y_train = np.reshape(Y_train, (len(Y_train), 1))
+            XY_train = np.hstack((X_train, Y_train))
+
         for i_epoch in range(epochs):                                # loop for each epoch
+            if (shuffle==True):
+                np.random.shuffle(XY_train)
+                X_train = XY_train[:, :-1]
+                Y_train = XY_train[:, -1]
+
+            if (i_epoch>=52):
+                a = 0
+
             for i_n_batch in range(n_batch+1):                        # loop for each batch
                 if (i_n_batch < n_batch):
                     for i_batch in range(batch_size):                # loop for each event inside batch
@@ -273,7 +320,7 @@ class NeuralNetwork():
                         self.propagation(y_train) 
                         corr_w += self.w_error 
                         corr_b += self.b_error 
-                        loss_train[i_epoch] += self.loss(y_train) 
+                        loss_train[i_epoch] += self.loss_func(y_train) 
                 elif (i_n_batch == n_batch):
                     for i_batch in range(n_train % batch_size):
                         x_train = np.copy(X_train[i_n_batch*batch_size+i_batch])
@@ -282,20 +329,20 @@ class NeuralNetwork():
                         self.propagation(y_train)
                         corr_w += self.w_error
                         corr_b += self.b_error
-                        loss_train[i_epoch] += self.loss(y_train) 
+                        loss_train[i_epoch] += self.loss_func(y_train) 
                 ##### corr_w and b need to be renormalized???
-                self.w += corr_w * self.lr
-                self.b += corr_b * self.lr
+                self.w -= corr_w * self.lr
+                self.b -= corr_b * self.lr
                 corr_w, corr_b = 0, 0
             for i_val in range(n_val):
                 self.predict(X_val[i_val])
                 y_val =  np.copy(Y_val[i_val])
-                loss_val[i_epoch] += self.loss(y_val)
+                loss_val[i_epoch] += self.loss_func(y_val)
             loss_train[i_epoch] = loss_train[i_epoch] / n_train
             loss_val[i_epoch] = loss_val[i_epoch] / n_val
             print("Epoch: {}/{}  ,  training loss: {}  ,  validation loss: {}".format(i_epoch+1, epochs, loss_train[i_epoch], loss_val[i_epoch]))
             if ((loss_val[i_epoch-1]-loss_val[i_epoch]) > min_improvement):
-                not_improved = 0
+                #not_improved = 0
                 if(loss_val[i_epoch] < loss_val[i_best]):
                     wgt_best = self.w                                # to save the best weights
                     i_best = i_epoch
